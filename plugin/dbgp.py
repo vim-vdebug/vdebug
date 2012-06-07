0147663dd57b1ee9a139c6a4041673b27d5b70b7
Jon Cairns <jon@22blue.co.uk>
2012-06-07 10:51:35 +0100

    Added Protocol class
    The Protocol class is used as a object-oriented interface to the
    debugger, using a DBGP protocol. Also, docstrings have been added.

--- plugin/dbgp.py
+++ plugin/dbgp.py
@@ -1,16 +1,46 @@
 import socket
 
+"""This module provides classes for speaking with debuggers that use the DBGP protocol.
+
+These classes are language independent, and can be used without external modules.
+"""
+
 class Connection:
-    """ DBGP connection class """
+    """DBGP connection class, for managing the connection to the debugger.
+
+    The host, port and socket timeout are configurable on object construction.
+    """
+
+    sock = None
+    isconned = 0
+
     def __init__(self, host = '', port = 9000, timeout = 30):
+        """Create a new Connection.
+
+        The connection is not established until open() is called.
+
+        host -- host name where debugger is running (default '')
+        port -- port number which debugger is listening on (default 9000)
+        timeout -- time in seconds to wait for a debugger connection before giving up (default 30)
+        """
         self.port = 9000
-        self.sock = None
         self.host = host
         self.timeout = timeout
-        self.isconned = 0
+
+    def __del__(self):
+        """Make sure the connection is closed."""
+        self.close()
+
     def isconnected(self):
+        """Whether the connection has been established."""
         return self.isconned
+
     def open(self):
+        """Listen for a connection from the debugger.
+
+        The socket is blocking, and it will wait for the length of
+        time given by the timeout (default is 30 seconds).
+        """
         print 'Waiting for a connection (this message will self-destruct in ',self.timeout,' seconds...)'
         serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
@@ -25,15 +55,19 @@ class Connection:
             self.stop()
             raise e
 
-        print 'Found connection from ', address
+        print 'Found connection from', address
         self.isconned = 1
         serv.close()
+
     def close(self):
+        """Close the connection."""
         if self.sock != None:
             self.sock.close()
             self.sock = None
         self.isconned = 0
-    def recv_length(self):
+
+    def __recv_length(self):
+        """Get the length of the proceeding message."""
         length = ''
         while 1:
             c = self.sock.recv(1)
@@ -44,7 +78,9 @@ class Connection:
                 return int(length)
             if c.isdigit():
                 length = length + c
-    def recv_null(self):
+
+    def __recv_null(self):
+        """Receive a null byte."""
         while 1:
             c = self.sock.recv(1)
             if c == '':
@@ -52,7 +88,12 @@ class Connection:
                 raise EOFError, 'Socket Closed'
             if c == '\0':
                 return
-    def recv_body(self, to_recv):
+
+    def __recv_body(self, to_recv):
+        """Receive a message of a given length.
+
+        to_recv -- length of the message to receive
+        """
         body = ''
         while to_recv > 0:
             buf = self.sock.recv(to_recv)
@@ -62,10 +103,67 @@ class Connection:
             to_recv -= len(buf)
             body = body + buf
         return body
+
     def recv_msg(self):
-        length = self.recv_length()
-        body     = self.recv_body(length)
-        self.recv_null()
+        """Receive a message from the debugger.
+        
+        Returns a string, which is expected to be XML.
+        """
+        length = self.__recv_length()
+        body     = self.__recv_body(length)
+        self.__recv_null()
         return body
+
     def send_msg(self, cmd):
+        """Send a message to the debugger.
+
+        cmd -- command to send
+        """
         self.sock.send(cmd + '\0')
+
+class Protocol:
+    """Interface for DBGP commands.
+
+    Uses a Connection object to read and write with the debugger,
+    and builds commands and returns the results.
+    """
+
+    conn = None
+    transID = 0
+
+    def __init__(self,connection):
+        """Create a new Protocol using a Connection object.
+
+        The Connection object specifies the debugger connection,
+        and the Protocol provides a OO interface to interacting
+        with it.
+
+        connection -- The Connection object to use
+        """
+        self.conn = connection
+        if self.conn.isconnected == 0:
+            self.conn.open()
+        self.init_msg = self.conn.recv_msg()
+
+    def send_cmd(self,cmd,args = ''):
+        """Send a command to the debugger.
+
+        This method automatically adds a unique transaction ID to the command,
+        which is required by the debugger.
+
+        cmd -- the command name, e.g. 'status'
+        args -- arguments for the command, which is optional for certain commands (default '')
+        """
+        args = args.strip()
+        cmd = cmd.strip()
+        self.transID += 1
+        cmd += ' -i '+ str(self.transID)
+        if len(args) > 0:
+            cmd += ' ' + args
+        print "Command: ",cmd
+        self.conn.send_msg(cmd)
+        return self.conn.recv_msg()
+
+    def status(self):
+        """Get the debugger status."""
+        return self.send_cmd('status')
