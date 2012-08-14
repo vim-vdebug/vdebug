e912abe1e560beac65d45c553850587c4874e592
Jon Cairns <jon@ggapps.co.uk>
2012-08-14 10:25:43 +0100

    Refactored exception handling and updated Ruby patch help

--- plugin/python/start_vdebug.py
+++ plugin/python/start_vdebug.py
@@ -5,6 +5,7 @@ import inspect
 dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 sys.path.append(dir)
 
+import socket
 import vim
 import traceback
 import vdebug.runner
@@ -43,175 +44,105 @@ class DebuggerInterface:
                 str(sys.exc_info()[0])+\
                 "\n"+ traceback.format_exc())
 
-    def run(self):
-        try:
-            self.runner.run()
-        except vdebug.dbgp.TimeoutError:
+    def handle_exception(self,e):
+        if isinstance(e,vdebug.dbgp.TimeoutError):
             self.handle_timeout()
-        except vdebug.log.LogError, e:
+        elif isinstance(e,vdebug.event.EventError):
+            self.runner.ui.error(e)
+        elif isinstance(e,vdebug.breakpoint.BreakpointError):
+            self.runner.ui.error(e)
+        elif isinstance(e,vdebug.log.LogError):
             self.handle_helpful_error(e)
-        except EOFError:
+        elif isinstance(e,(EOFError,socket.error)):
             self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except KeyboardInterrupt:
+        elif isinstance(e,KeyboardInterrupt):
             print "Keyboard interrupt - debugging session cancelled"
             try:
                 self.runner.close()
             except:
                 pass
-        except:
+        elif isinstance(e,vim.error):
+            self.handle_vim_error(e)
+        else:
             self.handle_general_exception()
+
+    def run(self):
+        try:
+            self.runner.run()
+        except Exception, e:
+            self.handle_exception(e)
 
     def run_to_cursor(self):
         try:
             self.runner.run_to_cursor()
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except vdebug.log.LogError, e:
-            self.handle_helpful_error(e)
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def handle_return_keypress(self):
         try:
             return self.event_dispatcher.by_position()
-        except vdebug.event.EventError,e :
-            self.runner.ui.error(e)
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def handle_double_click(self):
         try:
             return self.event_dispatcher.by_position()
-        except vdebug.event.EventError,e :
-            self.runner.ui.error(e)
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
-
+        except Exception, e:
+            self.handle_exception(e)
 
     def handle_visual_eval(self):
         try:
             return self.event_dispatcher.visual_eval()
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def handle_eval(self,args):
         try:
             return self.runner.eval(args)
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def toggle_breakpoint_window(self):
-        return self.runner.toggle_breakpoint_window()
+        try:
+            return self.runner.toggle_breakpoint_window()
+        except Exception, e:
+            self.handle_exception(e)
 
     def step_over(self):
         try:
             self.runner.step_over()
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except vdebug.log.LogError, e:
-            self.handle_helpful_error(e)
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def step_into(self):
         try:
             self.runner.step_into()
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except vdebug.log.LogError, e:
-            self.handle_helpful_error(e)
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def step_out(self):
         try:
             self.runner.step_out()
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except vdebug.log.LogError, e:
-            self.handle_helpful_error(e)
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def set_breakpoint(self,args = None):
         try:
             self.runner.set_breakpoint(args)
-        except vdebug.breakpoint.BreakpointError, e:
-            self.runner.ui.error(e)
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def remove_breakpoint(self,args = None):
         try:
             self.runner.remove_breakpoint(args)
-        except vdebug.breakpoint.BreakpointError, e:
-            self.runner.ui.error(e)
-        except vdebug.dbgp.TimeoutError:
-            self.handle_timeout()
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def detach(self):
         try:
             self.runner.detach()
-        except EOFError:
-            self.handle_socket_end()
-        except vim.error, e:
-            self.handle_vim_error(e)
-        except:
-            self.handle_general_exception()
+        except Exception, e:
+            self.handle_exception(e)
 
     def close(self):
         if self.runner.is_alive():
