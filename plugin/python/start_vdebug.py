5eb4f660e89ae1668c9b0e3ab4309a2076a3430d
Jon Cairns <jon@ggapps.co.uk>
2012-08-14 10:35:03 +0100

    Added comments to debugger interface

--- plugin/python/start_vdebug.py
+++ plugin/python/start_vdebug.py
@@ -12,9 +12,10 @@ import vdebug.runner
 import vdebug.event
 
 class DebuggerInterface:
-    """ Acts as an interface, and as an extra layer above the Runner class.
+    """Acts as a facade layer to the debugger client.
 
-    Fatal exceptions are caught and handled here.
+    Most methods are just redirected to the Runner class. Fatal 
+    exceptions are caught and handled here.
     """
     def __init__(self):
         self.runner = vdebug.runner.Runner()
@@ -23,36 +24,164 @@ class DebuggerInterface:
     def __del__(self):
         self.runner.close()
 
+    def run(self):
+        """Tell the debugger to run, until the next breakpoint or end of script.
+        """
+        try:
+            self.runner.run()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def run_to_cursor(self):
+        """Run to the current VIM cursor position.
+        """
+        try:
+            self.runner.run_to_cursor()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def step_over(self):
+        """Step over to the next statement.
+        """
+        try:
+            self.runner.step_over()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def step_into(self):
+        """Step into a statement on the current line.
+        """
+        try:
+            self.runner.step_into()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def step_out(self):
+        """Step out of the current statement.
+        """
+        try:
+            self.runner.step_out()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def handle_return_keypress(self):
+        """React to a <enter> keypress event.
+        """
+        try:
+            return self.event_dispatcher.by_position()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def handle_double_click(self):
+        """React to a mouse double click event.
+        """
+        try:
+            return self.event_dispatcher.by_position()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def handle_visual_eval(self):
+        """React to eval during visual selection.
+        """
+        try:
+            return self.event_dispatcher.visual_eval()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def handle_eval(self,args):
+        """Evaluate a code snippet specified by args.
+        """
+        try:
+            return self.runner.eval(args)
+        except Exception, e:
+            self.handle_exception(e)
+
+    def toggle_breakpoint_window(self):
+        """Open or close the breakpoint window.
+        """
+        try:
+            return self.runner.toggle_breakpoint_window()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def set_breakpoint(self,args = None):
+        """Set a breakpoint, specified by args.
+        """
+        try:
+            self.runner.set_breakpoint(args)
+        except Exception, e:
+            self.handle_exception(e)
+
+    def remove_breakpoint(self,args = None):
+        """Remove one or more breakpoints, specified by args.
+        """
+        try:
+            self.runner.remove_breakpoint(args)
+        except Exception, e:
+            self.handle_exception(e)
+
+    def detach(self):
+        """Detach the debugger, so the script runs to the end.
+        """
+        try:
+            self.runner.detach()
+        except Exception, e:
+            self.handle_exception(e)
+
+    def close(self):
+        """Close the connection, or the UI if already closed.
+        """
+        if self.runner.is_alive():
+            self.runner.close_connection()
+        else:
+            self.runner.close()
+
+    """ Exception handlers """
+
     def handle_timeout(self):
+        """Handle a timeout, which is pretty normal. 
+        """
         self.runner.close()
         self.runner.ui.say("No connection was made")
 
     def handle_socket_end(self):
+        """Handle a socket closing, which is pretty normal.
+        """
         self.runner.ui.say("Connection to the debugger has been closed")
         self.runner.close_connection()
 
     def handle_vim_error(self,e):
+        """Handle a VIM error.
+
+        This should NOT occur under normal circumstances.
+        """
         self.runner.ui.error("A Vim error occured: "+\
                 str(e)+\
                 "\n"+ traceback.format_exc())
 
-    def handle_helpful_error(self,e):
+    def handle_readable_error(self,e):
+        """Simply print an error, since it is human readable enough.
+        """
         self.runner.ui.error(str(e))
 
     def handle_general_exception(self):
+        """Handle an unknown error of any kind.
+        """
         self.runner.ui.error("An error occured: "+\
                 str(sys.exc_info()[0])+\
                 "\n"+ traceback.format_exc())
 
     def handle_exception(self,e):
+        """Switch on the exception type to work out how to handle it.
+        """
         if isinstance(e,vdebug.dbgp.TimeoutError):
             self.handle_timeout()
         elif isinstance(e,vdebug.event.EventError):
-            self.runner.ui.error(e)
+            self.handle_readable_error(e)
         elif isinstance(e,vdebug.breakpoint.BreakpointError):
-            self.runner.ui.error(e)
+            self.handle_readable_error(e)
         elif isinstance(e,vdebug.log.LogError):
-            self.handle_helpful_error(e)
+            self.handle_readable_error(e)
         elif isinstance(e,(EOFError,socket.error)):
             self.handle_socket_end()
         elif isinstance(e,KeyboardInterrupt):
@@ -65,87 +194,3 @@ class DebuggerInterface:
             self.handle_vim_error(e)
         else:
             self.handle_general_exception()
-
-    def run(self):
-        try:
-            self.runner.run()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def run_to_cursor(self):
-        try:
-            self.runner.run_to_cursor()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def handle_return_keypress(self):
-        try:
-            return self.event_dispatcher.by_position()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def handle_double_click(self):
-        try:
-            return self.event_dispatcher.by_position()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def handle_visual_eval(self):
-        try:
-            return self.event_dispatcher.visual_eval()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def handle_eval(self,args):
-        try:
-            return self.runner.eval(args)
-        except Exception, e:
-            self.handle_exception(e)
-
-    def toggle_breakpoint_window(self):
-        try:
-            return self.runner.toggle_breakpoint_window()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def step_over(self):
-        try:
-            self.runner.step_over()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def step_into(self):
-        try:
-            self.runner.step_into()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def step_out(self):
-        try:
-            self.runner.step_out()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def set_breakpoint(self,args = None):
-        try:
-            self.runner.set_breakpoint(args)
-        except Exception, e:
-            self.handle_exception(e)
-
-    def remove_breakpoint(self,args = None):
-        try:
-            self.runner.remove_breakpoint(args)
-        except Exception, e:
-            self.handle_exception(e)
-
-    def detach(self):
-        try:
-            self.runner.detach()
-        except Exception, e:
-            self.handle_exception(e)
-
-    def close(self):
-        if self.runner.is_alive():
-            self.runner.close_connection()
-        else:
-            self.runner.close()
