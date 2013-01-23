3b43f0e5b6f0d109dfd1fa56a8d554f07550f2ec
Jon Cairns <jon@joncairns.com>
2013-01-23 14:47:38 +0000

    Fix tests for breakpoint, raising exception as in #27

--- tests/test_breakpoint_breakpoint.py
+++ tests/test_breakpoint_breakpoint.py
@@ -108,9 +108,18 @@ class BreakpointTest(unittest.TestCase):
 
     def test_parse_with_line_breakpoint(self):
         """ Test that a LineBreakpoint is created."""
+        Mock.__len__ = Mock(return_value=1)
         ui = Mock()
         ret = vdebug.breakpoint.Breakpoint.parse(ui,"")
         self.assertIsInstance(ret,vdebug.breakpoint.LineBreakpoint)
+
+    def test_parse_with_empty_line_raises_error(self):
+        """ Test that a LineBreakpoint is created."""
+        Mock.__len__ = Mock(return_value=0)
+        ui = Mock()
+        re = 'Cannot set a breakpoint on an empty line'
+        self.assertRaisesRegexp(vdebug.breakpoint.BreakpointError,\
+                re,vdebug.breakpoint.Breakpoint.parse,ui,"")
 
     def test_parse_with_conditional_breakpoint(self):
         """ Test that a ConditionalBreakpoint is created."""
