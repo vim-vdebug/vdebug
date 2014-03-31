3d8f07e084b13e52acfde2689df2d2b52c77b93a
Jon Cairns <jon@joncairns.com>
2014-03-31 09:09:18 +0100

    Show local file path in stack, fix #118

--- plugin/python/vdebug/ui/vimui.py
+++ plugin/python/vdebug/ui/vimui.py
@@ -486,10 +486,10 @@ class StackGetResponseRenderer(ResponseRenderer):
         string = ""
         for s in stack:
             where = s.get('where') if s.get('where') else 'main'
-            file = vdebug.util.LocalFilePath(s.get('filename'))
+            file = vdebug.util.FilePath(s.get('filename'))
             line = "[%(num)s] %(where)s @ %(file)s:%(line)s" \
                     %{'num':s.get('level'),'where':where,\
-                    'file':str(file),'line':s.get('lineno')}
+                    'file':str(file.as_local()),'line':s.get('lineno')}
             string += line + "\n"
         return string
 
