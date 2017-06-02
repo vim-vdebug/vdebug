c079e129bd110c12f012f86fe0c2345c7c8d4f2c
Lucas Hoffmann <l-m-h@web.de>
2017-06-02 13:15:53 +0200

    Remove duplicated test for different log levels
    As the log level is handled by the base class there need not be
    different tests for different log levels in the child class.

--- tests/test_log.py
+++ tests/test_log.py
@@ -45,44 +45,23 @@ class LoggerTest(unittest.TestCase):
 
 class WindowLoggerTest(unittest.TestCase):
 
-    level = 2
+    level = 1
 
     def setUp(self):
         self.window = mock.Mock()
         self.logger = vdebug.log.WindowLogger(self.level, self.window)
 
-    def _log_tester(self, window_open, call_level, text='dummy text'):
-        self.window.is_open = window_open
-        ret = self.logger.log(text, call_level)
+    def test_log_with_open_window(self):
+        self.window.is_open = True
+        ret = self.logger.log('dummy text', self.level)
         self.assertIsNone(ret)
-
-    def test_log_with_same_level_and_open_window(self):
-        self._log_tester(True, self.level)
         self.window.create.assert_not_called()
         self.window.write.assert_called_once()
 
-    def test_log_with_higher_level_and_open_window(self):
-        self._log_tester(True, self.level+1)
-        self.window.create.assert_not_called()
-        self.window.write.assert_not_called()
-
-    def test_log_with_lower_level_and_open_window(self):
-        self._log_tester(True, self.level-1)
-        self.window.create.assert_not_called()
-        self.window.write.assert_called_once()
-
-    def test_log_with_same_level_and_no_window(self):
-        self._log_tester(False, self.level)
-        self.window.create.assert_called_once()
-        self.window.write.assert_called_once()
-
-    def test_log_with_higher_level_and_no_window(self):
-        self._log_tester(False, self.level+1)
-        self.window.create.assert_not_called()
-        self.window.write.assert_not_called()
-
-    def test_log_with_lower_level_and_no_window(self):
-        self._log_tester(False, self.level-1)
+    def test_log_with_no_window(self):
+        self.window.is_open = False
+        ret = self.logger.log('dummy text', self.level)
+        self.assertIsNone(ret)
         self.window.create.assert_called_once()
         self.window.write.assert_called_once()
 
