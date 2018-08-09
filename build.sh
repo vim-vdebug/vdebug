0c2df3e2a59aa371ce32dddf6e464c358b679cf6
Lucas Hoffmann <l-m-h@web.de>
2018-08-09 23:29:34 +0200

    Pack python3 instead of pythonx in the tarball

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 version=`cat VERSION`
 echo "Building vdebug version $version"
-tar -cvzf vdebug-$version.tar.gz doc plugin pythonx syntax CHANGELOG LICENCE README.md VERSION
+tar -cvzf vdebug-$version.tar.gz doc plugin python3 syntax CHANGELOG LICENCE README.md VERSION
